295d4b25364a29c1faa35c27da030ed1c99747d5
Luis31Prado <luis-prado-31@hotmail.com>
2020-06-01 20:54:06 -0500

    Se modifico el archivo

---
+++ listaingredientesRegistrar.php
@@ -0,0 +1,33 @@
+<?php
+
+    require("conexion.php");
+
+    // RECIBE LOS DATOS DE LA APP
+    $idproducto = $_POST['idproducto'];
+    $ingredientes = $_POST['ingredientes'];
+    $vitaminas = $_POST['vitaminas'];
+    $minerales = $_POST['minerales'];
+ 
+    
+   // VERIFICAMOS QUE NO ESTEN VACIAS LAS VARIABLES
+    if(empty($idproducto) || empty($ingredientes) || empty($vitaminas) || empty($minerales)) {
+
+        // SI ALGUNA VARIABLE ESTA VACIA MUESTRA ERROR
+        //echo "Se deben llenar los dos campos";
+        echo "ERROR 1, campos vacios";
+
+    } else {
+
+        // CREAMOS LA CONSULTA
+        $sql = "INSERT INTO listaingredinetes VALUES('', '$idproducto', '$ingredientes', '$vitaminas', '$minerales')";
+        $query = $mysqli->query($sql);
+
+        if($query === TRUE) {
+
+            echo "Guardado en BD";
+
+        }
+        
+    }
+
+?>
